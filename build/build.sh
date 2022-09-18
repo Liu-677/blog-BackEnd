c8fc7909db9c0ca5a80564eb739681a98db48071
liu677 <805415828@qq.com>
2022-09-18 21:35:32 +0800

    增加创建容器工能

--- build/build.sh
+++ build/build.sh
@@ -18,7 +18,7 @@ build_binary(){
 
 build_docker(){
   docker build -t blog-server:v1.0 $GOPATH/src/blog-BackEnd/build/docker/
-  docker run blog-server
+  docker run blog-server:v1.0
 }
 
 build_binary
